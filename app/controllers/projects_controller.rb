f697de62ca8b02a1ea372c1a1023749599023786
Ryan <ryan.t.robot@gmail.com>
2022-04-14 02:58:52 +0000

    Devise login iter 5

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+  def home
+    render "layouts/application"
+  end
 
   # GET /projects or /projects.json
   def index
